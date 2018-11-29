6ba112db9bece90baacaccbace1076deaf884711
mdamian9 <m.damian9@hotmail.com>
2018-11-29 14:36:00 -0800

    Fixed small error, added componentDidMount method

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -12,6 +12,10 @@ class Menu extends Component {
         };
     };
 
+    componentDidMount() {
+        console.log("Mounted");
+    };
+
     onDishSelect(dish) {
         this.setState({ selectedDish: dish });
     };
@@ -36,8 +40,8 @@ class Menu extends Component {
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-                <div className="col-12 col-md-5 m-1">
-                    <Card key={dish.id}
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card
                         onClick={() => this.onDishSelect(dish)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
