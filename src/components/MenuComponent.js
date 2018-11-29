9a41c731a9d03690ce8f778edf838406f6c8d9d0
mdamian9 <m.damian9@hotmail.com>
2018-11-29 14:16:45 -0800

    Added code to render dish when clicked, progress on MenuComponent

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -6,38 +6,61 @@ class Menu extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
 
+        this.state = {
+            selectedDish: null
         };
     };
 
-    render() {
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish });
+    };
 
+    renderDish(dish) {
+        if (dish != null)
+            return (
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return (
+                <div></div>
+            );
+    };
+
+    render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card tag="li">
-                        <CardImg width="100%" object src={dish.image} alt={dish.name} />
-                        <Media body className="ml-5">
-                            <Media heading>{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>
+                <div className="col-12 col-md-5 m-1">
+                    <Card key={dish.id}
+                        onClick={() => this.onDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
                     </Card>
                 </div>
-            )
+            );
         });
 
         return (
             <div className="container">
                 <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
+                    {menu}
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        {this.renderDish(this.state.selectedDish)}
+                    </div>
                 </div>
             </div>
         );
     };
-
 };
 
 export default Menu;
