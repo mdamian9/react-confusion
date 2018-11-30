5dec4ef8809fb58ff0f752da6a94c9ada1d5d053
mdamian9 <m.damian9@hotmail.com>
2018-11-29 19:44:15 -0800

    Made progress on DishdetailComponent, implemented in MenuComponent

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Media } from 'reactstrap';
 
 class DishDetail extends Component {
 
@@ -28,14 +28,48 @@ class DishDetail extends Component {
             );
     };
 
+    renderComments(commentsArr) {
+        if (commentsArr != null) {
+            const comments = commentsArr.map((comment) => {
+                return (
+                    <Media list key={comment.id} className="list-unstyled">
+                        <Media tag="li">
+                            <p>{comment.comment}</p>
+                        </Media>
+                        <Media tag="li">
+                            -- {comment.author}, {new Intl.DateTimeFormat('en-US',
+                                { year: 'numeric', month: 'long', day: '2-digit' }).format(new Date(comment.date))}
+                        </Media>
+                    </Media>
+                );
+            });
+            return (
+                <div>
+                    <Media>
+                        <h4>Comments</h4>
+                    </Media>
+                    {comments}
+                </div>
+            );
+        } else {
+            return (<div></div>);
+        }
+    };
+
     render() {
+
+        let comments;
+        if (this.props.dish != null) {
+            comments = this.renderComments(this.props.dish.comments);
+        };
+
         return (
             <div className="row">
                 <div className="col-12 col-md-5 m-1">
                     {this.renderDish(this.props.dish)}
                 </div>
                 <div className="col-12 col-md-5 m-1">
-
+                    {comments}
                 </div>
             </div>
         );
