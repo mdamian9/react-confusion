0570ec1d1fc149ecc84b4d043d58ed8efd490e40
mdamian9 <m.damian9@hotmail.com>
2018-12-04 11:19:21 -0800

    Installed redux and react-redux, created / set up reducer.js and configureStore.js

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,22 +6,12 @@ import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            dishes: DISHES,
-            comments: COMMENTS,
-            leaders: LEADERS,
-            promotions: PROMOTIONS
-        };
     };
 
     render() {
